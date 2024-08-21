2783d78332546978b1e0b08c91e5f811c83494de
zhao254014 <114656533+zhao254014@users.noreply.github.com>
2024-08-21 22:53:03 +0800

    Add files via upload

---
+++ ClusterGCF/utility/parser.py
@@ -0,0 +1,66 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run ClusterGCF.")
+    parser.add_argument('--weights_path', nargs='?', default=' ',
+                        help='Store model path.')
+    parser.add_argument('--data_path', nargs='?', default='../Data/',
+                        help='Input data path.')
+    parser.add_argument('--proj_path', nargs='?', default='',
+                        help='Project path.')
+
+    parser.add_argument('--dataset', nargs='?', default='office',
+                        help='Choose a dataset from {gowalla, KS10, office, yelp}')
+    parser.add_argument('--pretrain', type=int, default=0,
+                        help='0: No pretrain, -1: Pretrain with the learned embeddings, 1:Pretrain with stored models.')
+    parser.add_argument('--verbose', type=int, default=1,
+                        help='Interval of evaluation.')
+    parser.add_argument('--epoch', type=int, default=10,
+                        help='Number of epoch.')
+    parser.add_argument('--groups', type=int, default=1,
+                        help='Number of group.')
+
+    parser.add_argument('--temperature', type=float, default=0.1,
+                        help='Control probability distribution.')
+
+    parser.add_argument('--embed_size', type=int, default=64,
+                        help='Embedding size.')
+    parser.add_argument('--layer_size', nargs='?', default='[64]',
+                        help='Output sizes of every layer')
+    parser.add_argument('--batch_size', type=int, default=1024,
+                        help='Batch size.')
+    parser.add_argument('--regs', nargs='?', default='[1e-1]',
+                        help='Regularizations.')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='Learning rate.')
+
+    parser.add_argument('--model_type', nargs='?', default='ClusterGCF',
+                        help='Specify the name of model (ClusterGCF).')
+    parser.add_argument('--adj_type', nargs='?', default='pre',
+                        help='Specify the type of the adjacency (laplacian) matrix from {plain, norm, mean, pre}.')
+    parser.add_argument('--alg_type', nargs='?', default='ClusterGCF',
+                        help='Specify the type of the graph convolutional layer from {LightGCN,ClusterGCF}.')
+
+    parser.add_argument('--gpu_id', type=int, default=0,
+                        help='0 for NAIS_prod, 1 for NAIS_concat')
+
+    parser.add_argument('--node_dropout_flag', type=int, default=0,
+                        help='0: Disable node dropout, 1: Activate node dropout')
+    parser.add_argument('--node_dropout', nargs='?', default='[0.1]',
+                        help='Keep probability w.r.t. node dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
+    parser.add_argument('--mess_dropout', nargs='?', default='[0.1]',
+                        help='Keep probability w.r.t. message dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
+
+    parser.add_argument('--mlp_dropout', nargs='?', default='1.0',
+                        help='Keep probability of MLP layer')
+
+    parser.add_argument('--Ks', nargs='?', default='[20, 40, 60, 80, 100]',
+                        help='Output sizes of every layer')
+
+    parser.add_argument('--save_flag', type=int, default=1,
+                        help='0: Disable model saver, 1: Activate model saver')
+
+    parser.add_argument('--test_flag', nargs='?', default='part',
+                        help='Specify the test type from {part, full}, indicating whether the reference is done in mini-batch')
+
+    return parser.parse_args()
